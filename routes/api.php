ba0a1194913bf0e84f2ec3e9db6b8dc132215668
havvtom <havvtom@gmail.com>
2020-07-22 12:38:40 +0200

    logout api route

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@
 Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
 	Route::get('me', 'MeController');
 	Route::post('signin', 'SignInController');
+	Route::post('signout', 'SignOutController');
 });
